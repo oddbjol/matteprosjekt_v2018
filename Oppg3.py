c694bbf0cb7f3058b472a94147dff0b31847c2f4
odd <olsen.oddbjorn@gmail.com>
2018-03-09 21:45:13 +0100

    litt rydding

--- Oppg3.py
+++ Oppg3.py
@@ -1,15 +1,6 @@
-import scipy as sp
-import numpy as np
-
 from Stupebrett import Stupebrett
-
-n = 20 # antall segmenter
-h = 2/20 #segmentlengde
 
 brett = Stupebrett(L=2, w=0.3, d=0.03, p=480, E=1.3*10**10)
 
-y = brett.solve(20)
-
-
-print("beregnet y-vektor: ", y)
+print("beregnet y-vektor: ", brett.solve(20))
 print("y-vektor fra fasit: ", brett.correct_displacement(20))
