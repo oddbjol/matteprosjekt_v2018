75a1b22742497830e5cea64d52cca2af57adb22c
KimRostgard <max.ntnu@gmail.com>
2018-03-20 18:24:30 +0100

    Oppg4c

---
+++ Oppg4c.py
@@ -0,0 +1,30 @@
+from Stupebrett import Stupebrett
+import numpy as np
+
+
+stupebrett = Stupebrett()
+
+
+
+
+def main():
+    n = 10
+    h=0.2
+    Ye_vector = stupebrett.fasit_y(n)
+    A = stupebrett.lagA(n)
+    AYvector = np.dot(A.toarray(), Ye_vector)
+    Yx_fourthDerivative = AYvector/ h**4
+
+    print("The Ye vector: ", Ye_vector)
+    print("The A matrix: ", A)
+    print("Yx fourthDerivative: ", Yx_fourthDerivative)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
