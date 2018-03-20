9f4cbc530e6b0c8c463112d426c20f89e87bfad6
KimRostgard <max.ntnu@gmail.com>
2018-03-20 19:46:25 +0100

    continuing 4d

--- Oppg4c.py
+++ Oppg4c.py
@@ -10,18 +10,39 @@ stupebrett = Stupebrett()
 def main():
     n = 10
     h=0.2
+
+    # Oppgave 4c
+    # y''''(c) = (1/h^4) * AYe
     Ye_vector = stupebrett.fasit_y(n)
     A = stupebrett.lagA(n)
-    AYvector = np.dot(A.toarray(), Ye_vector)
-    Yx_fourthDerivative = AYvector/ h**4
+    AYvector = np.dot(A, Ye_vector)
+    Yc_fourthDerivative = AYvector/ h**4
 
     print("The Ye vector: ", Ye_vector)
     print("The A matrix: ", A)
-    print("Yx fourthDerivative: ", Yx_fourthDerivative)
+    print("Yc fourthDerivative: ", Yc_fourthDerivative)
+
+    # Oppgave 4d
+    # y''''(e) = (b/h^4)
+    # Foroverfeil = ||Ye - Yc||
+    b_vector = stupebrett.lagB(n)
+    Ye_fourthDerivative = b_vector/ h**4
+    print("The b-vector: ", b_vector)
+    print("Ye fourthDerivative: ", Ye_fourthDerivative)
+
+    diff = Ye_vector - b_vector
+    print("Forward error: ", diff)
 
 
-    #b_vector = stupebrett.lagB(n)
-    #print("The b-vector: ", b_vector)
+
+
+
+
+
+
+
+
+
 
 
 
