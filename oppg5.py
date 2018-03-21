b1b6cca9b4dc4b17aa2a8b852c5aac0bfa60dc61
eirik <stenseneirik@gmail.com>
2018-03-22 00:38:00 +0100

    Lagd metode som finner størst x = L (ytters på stupebrettet)

--- oppg5.py
+++ oppg5.py
@@ -3,29 +3,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 def main():
 
     brett = Stupebrett()
 
-    feil_i_L = np.zeros(12)
-    x = np.zeros(12)
-    laga = np.zeros(12)
+    # global x
+    # global kondisjonsTallA
+
+    # feil_i_L = np.zeros(12)
+    # x = np.zeros(12)
+
 
     for i in range(0, 12):
         n = 20 * (2**i)
 
-        x[i] = n
-        feil_i_L[i] = np.abs(brett.fasit_y(n) - brett.finn_y(n))[n - 1]
-        laga = brett.lagA(i)
-        # print(i, feil_i_L)
+        #x[i] = n
+        feil_i_L = np.abs(brett.fasit_y(n) - brett.finn_y(n))[n - 1]
+        kondisjonsTallA = np.linalg.cond(brett.lagA(n).toarray())
+        print(n, feil_i_L, "\t", "condition", kondisjonsTallA)
 
-    for i in range(11 + 1):
-        print(x[i], "\t", feil_i_L, "\t", brett.lagA(x))
 
-    plt.plot(x, feil_i_L, color='blue')
-    plt.plot(x, laga, color='red')
-    plt.show()
+        #print(i, feil_i_L)
+        # print(kondisjonsTallA)
+        # plt.plot(kondisjonsTallA)
+
+
+    # for i in range(11 + 1):
+    #     print(x[i], "\t", "N in", feil_i_L, "\t", "Feil")
+
+    # print(np.linalg.cond(brett.lagA(x).toarray(x)))
+    # print(kondisjonsTallA)
+
+    #plt.plot(x, feil_i_L, color='blue')
+
+    #plt.show()
 
 
 if __name__ == '__main__':
